f07ad09ee637a9c5ca307f32087245a7904ccecc
Charlie <dev@dev.com>
2018-11-04 15:48:59 -0800

    Testing deployed contract methods

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -11,11 +11,21 @@ beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     inbox = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas : '1000000'})
+        .send({ from: accounts[0], gas : '1000000'});
 })
 
 describe('Inbox', () => {
     it('deploys contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there!');
+    });
+    it('can change a message', async () => {
+        await inbox.methods.setMessage('bye').send({from : accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert(message, 'bye');
+    })
 })
